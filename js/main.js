2019a86fca6accff74ca89543dcf1b959525c2d4
DanielJMaia <djohnsonmaia@gmail.com>
2019-10-23 16:27:36 +0000

    successfully printing API to console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-var xhr = new XMLHttpRequest();
+   function getData(type, cb) {
+       var xhr = new XMLHttpRequest();
+       xhr.open("GET", "https://api.magicthegathering.io/v1/cards/1");
+       xhr.send();
+
+       xhr.onreadystatechange = function() {
+           if (this.readyState == 4 && this.status == 200) {
+               cb(JSON.parse(this.responseText));
+           }
+       };
+   }
+   
+   function writeToDocument(type) {
+       getData(type, function(data) {
+           document.getElementById("data").innerHTML = data;
+       })
+   }
