a25240e37dafa6c65938da515c4e145abae58b07
DanielJMaia <djohnsonmaia@gmail.com>
2019-10-24 15:22:23 +0000

    displays 10 cards that match the input

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
    const baseURL = "https://api.magicthegathering.io/"
-   
+
    function getData(type, cb) {
        var request = new XMLHttpRequest();
-       
+
        request.open("GET", baseURL + type);
        request.send();
 
@@ -12,31 +12,45 @@
            }
        };
    }
-   
-   function writeIdToDocument(type) {
-       getData(type, function(data) {
-           console.dir(data);
-           document.getElementById("data").innerHTML += data.card.artist;
-       });
-   }
-   
-   function writeNameToDocument(type) {
-       getData(type, function(data) {
-           console.dir(data);
-           document.getElementById("data").innerHTML += data.cards[0].cmc;
-       });
-   }
-   
-   
-  
-   function grabId(){
+
+
+   //Retrieval Functions
+
+   function grabId() {
        // This line retrieves the value of the user input i.e. the card ID and prints it to console for debugging
        var idValue = document.getElementById("cardId").value;
        console.log(idValue);
-       document.getElementById("buttonId").onclick = writeIdToDocument(`v1/cards/${idValue}`);
-     
+       writeIdToDocument(`v1/cards/${idValue}`);
    }
-   
-   
-  
+
+   function writeIdToDocument(type) {
+       getData(type, function(data) {
+           console.dir(data);
+
+           var i = 0;
+           for (i = 0; i < 10; i++) {
+               document.getElementById("data").innerHTML += data.card.name;
+           }
+
+       });
+   }
+
+
+   function grabName() {
+       var nameValue = document.getElementById("cardName").value;
+       console.log(nameValue);
+       writeNameToDocument(`v1/cards?name=${nameValue}`);
+   }
+
+   function writeNameToDocument(type) {
+       getData(type, function(data) {
+           console.dir(data);
+
+           var i = 0;
+           for (i = 0; i < 10; i++) {
+               document.getElementById("data").innerHTML += "<p>" + data.cards[i].cmc + "<p>";
+           }
+
+       });
+   }
    
