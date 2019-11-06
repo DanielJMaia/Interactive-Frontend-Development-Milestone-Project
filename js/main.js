ebcd29fd5b0f38cf38f15a9b9ed24a60fe194b10
DanielJMaia <djohnsonmaia@gmail.com>
2019-11-06 17:17:35 +0000

    removed visibility tags and changed order of code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
    //Get Data From API //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-   const proxyurl = "https://cors-anywhere.herokuapp.com/"
    const baseURL = "https://api.magicthegathering.io/"
 
    function getData(type, cb) {
@@ -40,8 +39,8 @@
       // Getting the card data
       getData(type, function(data) {
          console.dir(data);
-         var i = 0;
-         for (i = 0; i < 5; i++) {
+         
+         for (var i = 0; i <= data.cards.length; i++) {
             // Creating the Divs
 
             var cardRow = document.createElement("div");
@@ -50,7 +49,6 @@
             var imageDiv = document.createElement("div");
             imageDiv.setAttribute("class", "col-6 image");
             imageDiv.setAttribute("align", "right");
-            imageDiv.setAttribute("style", "visibility: hidden;");
 
             var imageSrc = document.createElement("img");
             imageSrc.setAttribute("class", "img-fluid");
@@ -61,7 +59,7 @@
             var card = document.createElement("div");
             card.setAttribute("class", "col-6 card");
             card.setAttribute("id", "cardMain" + i);
-            card.setAttribute("style", "width: 18rem; visibility: hidden;");
+            card.setAttribute("style", "width: 18rem;");
 
             var body = document.createElement("div");
             body.setAttribute("class", "card-body");
@@ -118,17 +116,18 @@
             cardRow.appendChild(card);
 
             document.getElementById("cardData").appendChild(cardRow);
+            
             // Clearing the innerHTML of new results so that they don't keep adding on when a new card is inputted
-            var innerId = document.getElementById("cardTitle");
-            var innerId1 = document.getElementById("cardType");
-            var innerId2 = document.getElementById("cardCost");
-            var innerId3 = document.getElementById("cardText");
-            var innerId4 = document.getElementById("cardFlavour");
-            var innerId5 = document.getElementById("cardRarity");
-            var innerId6 = document.getElementById("cardPower");
-            var innerId7 = document.getElementById("cardArtist");
-            var innerId8 = document.getElementById("cardSet");
-            var innerId9 = document.getElementById("cardImage");
+            var innerId = document.getElementById("cardTitle" + i);
+            var innerId1 = document.getElementById("cardType" + i);
+            var innerId2 = document.getElementById("cardCost" + i);
+            var innerId3 = document.getElementById("cardText" + i);
+            var innerId4 = document.getElementById("cardFlavour" + i);
+            var innerId5 = document.getElementById("cardRarity" + i);
+            var innerId6 = document.getElementById("cardPower" + i);
+            var innerId7 = document.getElementById("cardArtist" + i);
+            var innerId8 = document.getElementById("cardSet" + i);
+            var innerId9 = document.getElementById("cardImage" + i);
 
             innerId.innerHTML = `<img src="images/loader.gif">`;
             innerId1.innerHTML = "";
@@ -141,9 +140,7 @@
             innerId8.innerHTML = "";
             innerId9.src = "images/loader.gif";
 
-            //Setting Visibility
-            document.getElementById("cardImage" + i).style.visibility = "visible";
-            document.getElementById("cardMain" + i).style.visibility = "visible";
+           // Filling up the respecting Div with the data
 
             document.getElementById("cardTitle" + i).innerHTML = data.cards[i].name;
             document.getElementById("cardType" + i).innerHTML += "Type: " + data.cards[i].originalType;
@@ -159,84 +156,3 @@
 
       });
    }
-
-   function createDiv() {
-      for (var i = 0; i < 101; i++) {
-
-         var cardRow = document.createElement("div");
-         cardRow.setAttribute("class", "row");
-
-         var imageDiv = document.createElement("div");
-         imageDiv.setAttribute("class", "col-6 image");
-         imageDiv.setAttribute("align", "right");
-         imageDiv.setAttribute("style", "visibility: hidden;");
-
-         var imageSrc = document.createElement("img");
-         imageSrc.setAttribute("class", "img-fluid");
-         imageSrc.setAttribute("id", "cardImage" + i);
-         imageSrc.setAttribute("src", "");
-         imageSrc.setAttribute("alt", "Card Image Not Available");
-
-         var card = document.createElement("div");
-         card.setAttribute("class", "col-6 card");
-         card.setAttribute("id", "cardMain" + i);
-         card.setAttribute("style", "width: 18rem; visibility: hidden;");
-
-         var body = document.createElement("div");
-         body.setAttribute("class", "card-body");
-
-         var header = document.createElement("h5");
-         header.setAttribute("class", "card-title");
-         header.setAttribute("id", "cardTitle" + i);
-
-         var paragraphType = document.createElement("p");
-         paragraphType.setAttribute("class", "card-text");
-         paragraphType.setAttribute("id", "cardType" + i);
-
-         var paragraphCost = document.createElement("p");
-         paragraphCost.setAttribute("class", "card-text");
-         paragraphCost.setAttribute("id", "cardCost" + i);
-
-         var paragraphText = document.createElement("p");
-         paragraphText.setAttribute("class", "card-text");
-         paragraphText.setAttribute("id", "cardText" + i);
-
-         var paragraphFlavour = document.createElement("p");
-         paragraphFlavour.setAttribute("class", "card-text");
-         paragraphFlavour.setAttribute("id", "cardFlavour" + i);
-
-         var paragraphRarity = document.createElement("p");
-         paragraphRarity.setAttribute("class", "card-text");
-         paragraphRarity.setAttribute("id", "cardRarity" + i);
-
-         var paragraphPower = document.createElement("p");
-         paragraphPower.setAttribute("class", "card-text");
-         paragraphPower.setAttribute("id", "cardPower" + i);
-
-         var paragraphArtist = document.createElement("p");
-         paragraphArtist.setAttribute("class", "card-text");
-         paragraphArtist.setAttribute("id", "cardArtist" + i);
-
-         var paragraphSet = document.createElement("p");
-         paragraphSet.setAttribute("class", "card-text");
-         paragraphSet.setAttribute("id", "cardSet" + i);
-
-         imageDiv.appendChild(imageSrc);
-         body.appendChild(header);
-         body.appendChild(paragraphType);
-         body.appendChild(paragraphCost);
-         body.appendChild(paragraphText);
-         body.appendChild(paragraphFlavour);
-         body.appendChild(paragraphRarity);
-         body.appendChild(paragraphPower);
-         body.appendChild(paragraphArtist);
-         body.appendChild(paragraphSet);
-         card.appendChild(body);
-
-         cardRow.appendChild(imageDiv);
-         cardRow.appendChild(card);
-
-         document.getElementById("cardData").appendChild(cardRow);
-      }
-   }
-   
